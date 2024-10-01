37a973c0712946e3e2ac61fc3c3d361ae27368d3
Matias Ariel Coco <cocomatias6@gmail.com>
2024-10-01 18:42:20 -0300

    update login footer y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: HomeComponent },
+    // { path: 'products', component: ProductComponent },
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },  
+    { path: '**', redirectTo: '', pathMatch: 'full' } 
+];
