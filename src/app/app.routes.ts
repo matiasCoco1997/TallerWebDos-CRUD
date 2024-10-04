2f4b1790d773164899676fa9c9c953b3a1c07647
Matias Ariel Coco <cocomatias6@gmail.com>
2024-10-03 23:57:54 -0300

    update login avanzado
     se importo routerlink y dio forma al footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
-    // { path: '', component: HomeComponent },
-    // { path: 'products', component: ProductComponent },
     { path: '', component: LoginComponent },
-    { path: 'login', component: LoginComponent },  
-    { path: '**', redirectTo: '', pathMatch: 'full' } 
+    { path: 'login', component: LoginComponent },
+    { path: 'registrarse', component: SignUpComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
 
 }
